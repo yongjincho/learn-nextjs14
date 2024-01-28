424a952d9af58ca3112d48625896cd1b513d5599
Yongjin Cho <yongjin.cho@gmail.com>
2024-01-28 16:09:15 +0900

    initial

---
+++ app/about-us/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'About us',
+}
+
+export default function Page() {
+  return (
+    <div>
+      <h1>About us!</h1>
+    </div>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "404 - Page Not Found",
+};
+
+export default function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
+}
